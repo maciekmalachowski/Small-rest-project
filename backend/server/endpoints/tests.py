21f2d17009d95ca9ea1f4fa544176d37d4fc417d
MaciekEO <malas12x09@gmail.com>
2022-03-11 10:05:51 +0100

    👀test edit

--- backend/server/endpoints/tests.py
+++ backend/server/endpoints/tests.py
@@ -21,37 +21,37 @@ class TestUrl(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.json()), 5)
     
-    def test_url_1(self):
+    def test_url_incomplite_link(self):
         client = RequestsClient()
         response = client.get('http://127.0.0.1:8000')
         self.assertEqual(response.status_code, 404)
 
-    def test_url_2(self):
+    def test_url_single_minus_value(self):
         client = RequestsClient()
         response = client.get('http://127.0.0.1:8000/get-data/-1')
         self.assertEqual(response.status_code, 400)
 
-    def test_url_3(self):
+    def test_url_single_not_int(self):
         client = RequestsClient()
         response = client.get('http://127.0.0.1:8000/get-data/a')
         self.assertEqual(response.status_code, 400)
 
-    def test_url_4(self):
+    def test_url_intmin_bigger_than_intmax(self):
         client = RequestsClient()
         response = client.get('http://127.0.0.1:8000/get-data/2/1')
         self.assertEqual(response.status_code, 400)
     
-    def test_url_5(self):
+    def test_url_intmax_minus_value(self):
         client = RequestsClient()
-        response = client.get('http://127.0.0.1:8000/get-data/2/-1')
+        response = client.get('http://127.0.0.1:8000/get-data/1/-1')
         self.assertEqual(response.status_code, 400)
 
-    def test_url_6(self):
+    def test_url_intmin_minus_value(self):
         client = RequestsClient()
-        response = client.get('http://127.0.0.1:8000/get-data/-2/1')
+        response = client.get('http://127.0.0.1:8000/get-data/-1/1')
         self.assertEqual(response.status_code, 400)
     
-    def test_url_7(self):
+    def test_url_many_not_int(self):
         client = RequestsClient()
         response = client.get('http://127.0.0.1:8000/get-data/a/b')
         self.assertEqual(response.status_code, 400)
